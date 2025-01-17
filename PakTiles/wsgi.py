26d63c29977481f8db9b4c9f45bf6e295ed84a7d
aijazabbasi <aijaz.engr@gmail.com>
2025-01-18 04:02:00 +0500

    Update wsgi.py

--- PakTiles/wsgi.py
+++ PakTiles/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PakTiles.settings')
 
